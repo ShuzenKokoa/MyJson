700817153c566e753abd8efba9de4c0706833cfb
ShuzenKokoa <46623083+ShuzenKokoa@users.noreply.github.com>
2024-01-28 19:42:13 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,27 +13,29 @@ int main() {
     j3.append("1");
     j3.append(j2);
 
-
+    std::cout<<j3.str()<<'\n';
+//
     Json j5;
     j5["ddd"] = "obj";
-    Json j4;
-    j4.append("arr");
-    j4.append(j5);
 
-    j3.append(j4);
-    std::cout << j3.str();
-
-    std::ifstream file("example.txt");
-    std::ostringstream oss;
-    oss << file.rdbuf();
-    std::string str = oss.str();
+    std::cout<<j5.str()<<'\n';
+//    j4.append(j5);
+//
+//    j3.append(j4);
+//    std::cout << j3.str();
+//
+    std::ifstream file("./example.txt");
+    std::stringstream ss;
+    ss << file.rdbuf();
+    const std::string& s = ss.str();
     std::cout<<"_____"<<std::endl;
-
-    std::cout<<str<<std::endl;
-    std::cout<<"_____"<<std::endl;
-
+//    std::cout<<ss<<'\n';
+//
+//    std::cout<<str<<std::endl;
+//    std::cout<<"_____"<<std::endl;
+//
     Json test1;
-    test1.parse(str);
+    test1.parse(s);
     std::cout<<test1.str();
-
+    system("pause");
 }
